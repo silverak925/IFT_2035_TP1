36b2ccc23d119dd25b51e9ae4abcb35ae40fab8c
Karim <silverak925@gmail.com>
2024-10-16 07:47:01 -0400

    Completition

--- slip.hs
+++ slip.hs
@@ -198,87 +198,23 @@ s2l (Ssym s) = Lvar s
 -- Analyse des Snodes possibles
 s2l (Snode (Ssym "if") [condition, trueBranch, falseBranch]) =
   -- On evalue les deux possibles outcomes
-  Ltest (s2l cond) (s2l trueBranch) (s2l falseBranch)
+  Ltest (s2l condition) (s2l trueBranch) (s2l falseBranch)
 
 s2l (Snode (Ssym "let") [Ssym var, expr, body]) =
   Llet var (s2l expr) (s2l body)
 
-s2l (Snode (Ssym "lambda") [Snode params, body]) =
-  Lfob (map s2lParam params) (s2l body)
+s2l (Snode (Ssym "lambda") [Snode (Ssym "params") paramList, body]) =
+  Lfob (map s2lParam paramList) (s2l body)
   where
     s2lParam (Ssym v) = v
-    s2lParam _ = error "Parametre lambda invalide"
+    s2lParam _ = error "Paramètre lambda invalide"
 
 s2l (Snode func args) = Lsend (s2l func) (map s2l args)
 
+s2l Snil = error "Liste vide inattendue"
+
 -- Erreur pour tout autre expression inconnue
-s2l _ = error "Expression Sexp inconnue: " ++ show se)"
-
-{-
--- Helper function to extract Int from Lnum
-getNum :: Lexp -> Maybe Int
-getNum (Lnum n) = Just n
-getNum _        = Nothing
-
-s2l (Snode (Ssym "+") listExp) =
-    let args = map s2l listExp  -- Applique récursivement `s2l` à chaque sous-expression
-    in case sequenceA (map getNum args) of
-        Just nums -> Lnum (sum nums)  -- Si tous sont des entiers constants, calcule la somme
-        Nothing   -> Lsend (Lvar "+") args  -- Sinon, différer l'évaluation avec `Lsend`
-
-s2l (Snode (Ssym "-") listExp) =
-    let args = map s2l listExp
-    in case sequenceA (map getNum args) of
-        Just (x:xs) -> Lnum (x - sum xs)  -- Pour la soustraction, on soustrait la somme des autres
-        Nothing      -> Lsend (Lvar "-") args
-
-s2l (Snode (Ssym "*") listExp) =
-    let args = map s2l listExp
-    in case sequenceA (map getNum args) of
-        Just nums -> Lnum (product nums)  -- Produit des entiers
-        Nothing   -> Lsend (Lvar "*") args
-
-s2l (Snode (Ssym "/") listExp) =
-    let args = map s2l listExp
-    in case sequenceA (map getNum args) of
-        Just (x:xs) -> Lnum (foldl (\acc n -> acc `div` n) x xs)  -- Division
-        Nothing      -> Lsend (Lvar "/") args
-
---Prend les boolean en arguments
-getBool :: Lexp -> Maybe Bool
-getBool (Lbool b) = Just b
-getBool _         = Nothing
-
-s2l (Snode (Ssym "and") listExp) =
-    let args = map s2l listExp
-    in case sequenceA (map getBool args) of
-        Just bools -> Lbool (and bools)
-        Nothing    -> Lsend (Lvar "and") args
-
-s2l (Snode (Ssym "or") listExp) =
-    let args = map s2l listExp
-    in case sequenceA (map getBool args) of
-        Just bools -> Lbool (or bools)
-        Nothing    -> Lsend (Lvar "or") args
-
-s2l (Snode (Ssym "not") [e]) =
-    let arg = s2l e
-    in case arg of
-        Lbool b -> Lbool (not b)
-        _       -> Lsend (Lvar "not") [arg]
-
-s2l (Snode (Ssym "let") [Snode [Snode [Ssym var, e1]], e2]) = 
-    Llet var (s2l e1) (s2l e2)
-
-s2l (Snode (Ssym "fix") [Snode bindings, body]) =
-    Lfix (map (\(Snode [Ssym v, e]) -> (v, s2l e)) bindings) (s2l body)        
-
-s2l (Snode _ _) = error "Expression Sexp inconnue"
-s2l Snil = Lsend (Lvar "nil") []  -- Cas pour la liste vide
-s2l se = error ("Expression Sexp inconnue: " ++ show se)
-
--}
-
+s2l se = error ("Expression Psil inconnue: " ++ showSexp se)
 
 ---------------------------------------------------------------------------
 -- Représentation du contexte d'exécution                                --
@@ -326,32 +262,47 @@ eval :: VEnv -> Lexp -> Value
 -- ¡¡ COMPLETER !!
 eval _ (Lnum n) = Vnum n
 
+-- On cherche value d'une variable
 eval env (Lvar v) =
   case lookup v env of
     Just val -> val
     Nothing -> error ("Variable non définie: " ++ v)
+
+-- Evaluation if
 eval env (Ltest cond trueBranch falseBranch) =
   case eval env cond of
     Vbool True  -> eval env trueBranch
     Vbool False -> eval env falseBranch
     _ -> error "Condition must evaluate to a boolean"
+
+eval _ (Lbool b) = Vbool b
+eval env (Lfob params body) = Vfob env params body
+
+-- Évaluation d'un appel de fonction ou d'un objet fonctionnel.
 eval env (Lsend func args) =
   case eval env func of
     Vbuiltin f -> f (map (eval env) args)
     Vfob closure params body ->
       let argVals = map (eval env) args
-          extendedEnv = zip params argVals ++ closure
-      in eval extendedEnv body
-    _ -> error "Application requires a function"
+      in if length params == length argVals  -- Vérification du nombre d'arguments.
+         then let extendedEnv = zip params argVals ++ closure
+              in eval extendedEnv body
+         else error "Nombre d'arguments incorrect pour la fonction"
+    _ -> error "Application nécessite une fonction"
+
+-- Evaluation let
 eval env (Llet v e1 e2) =
   let val = eval env e1
       extendedEnv = (v, val) : env
   in eval extendedEnv e2
+
+--evaluation fix
 eval env (Lfix bindings body) =
   let recEnv = [(v, eval recEnv e) | (v, e) <- bindings] ++ env
   in eval recEnv body
-                  
----------------------------------------------------------------------------
+  
+
+-------------------------------------------------
 -- Toplevel                                                              --
 ---------------------------------------------------------------------------
 
